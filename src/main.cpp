e509e8c852ae262e3fa40a12b1951d5d5d94b432
Trixie Roque <trixie.anne.roque@gmail.com>
2022-10-24 00:37:34 -0700

    Change to using AsyncWebServer

--- src/main.cpp
+++ src/main.cpp
@@ -1,62 +1,78 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
-#include <FS.h>
 #include <FastLED.h>
-#include <ESP8266WebServer.h>
-#include <WiFiClient.h>
+#include <ESPAsyncWebServer.h>
+#include <ESPAsyncTCP.h>
 
 #ifndef AP_SSID
 #define AP_SSID "trix-sims"
 #define AP_PSK "plumbob-69420"
 #endif
 
-#define LED_COUNT 14
 #define CONTROL_PIN D1
 
 // TODO: send slider input from Blynk App? to esp32
-// TODO: create qr code for connecting to hidden AP on esp32
-// TODO: set up webserver on nodemcu
 // TODO: set up client for phones
+// TODO: add button for power on/off
+// TODO: get slider input from slider component
+// TODO: add brightness input
+// TODO: add effects? (like spinning diamond)
 
 char ssid[] = AP_SSID;
 char password[] = AP_PSK;
 
-ESP8266WebServer server(80);
+const int LED_COUNT = 14;
 
-const uint8_t slider_val = 255;
+AsyncWebServer server(80);
+
+uint8_t slider_val = 0;
+uint8_t num_clients = 0;
+
+const char* SLIDER_INPUT = "slider";
 
 CRGBArray<LED_COUNT> leds;
-uint8_t brightness = 50;
+uint8_t brightness = 100;
 
-void handleHome() {
+void handleHome(AsyncWebServerRequest *req) {
   digitalWrite(CONTROL_PIN, HIGH);
-  server.send(200, "text/plain", "Welcome!\r\n");
+  req->send(200, "text/plain", "Welcome!\r\n");
   digitalWrite(CONTROL_PIN, LOW);
 }
 
-void handleNotFound() {
+void handleNotFound(AsyncWebServerRequest *req) {
   digitalWrite(CONTROL_PIN, HIGH);
-  String msg = "File Not Found\n\n";
-  msg += "URI: ";
-  msg += server.uri();
-  msg += "\nMethod: ";
-  msg += (server.method() == HTTP_GET) ? "GET" : "POST";
-  msg += "\nArguments: ";
-  msg += server.args();
-  msg += "\n";
-  for (uint8_t i = 0; i < server.args(); i++) { 
-    msg += " " + server.argName(i) + ": " + server.arg(i) + "\n"; 
-  }
-  server.send(404, "text/plain", msg);
+  req->send(404, "text/plain", "Page does not exist");
   digitalWrite(CONTROL_PIN, LOW);
 }
+
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html>
+  <head>
+    <title>The Sims Plumbob LED Controller</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head>
+  <body>
+    <h2>The Sims Plumbob LED Controller</h2>
+    <form action="/get">
+      <input type="range" name="slider" min="0" max="255" value="0">
+      <label for="slider">Slider</label><br>
+
+      <span name="num_clients"></span>
+      <label for="num_clients">Clients: </label>
+    </form><br>
+  <script>
+    var xhr = new XMLHttpRequest();
+  </script>
+  </body>
+</html>)rawliteral";
  
 void setup() {
+  pinMode(CONTROL_PIN, OUTPUT);
   Serial.begin(9600);
   LEDS.setBrightness(brightness);
   LEDS.addLeds<WS2812B, CONTROL_PIN, GRB>(leds, LED_COUNT);
   LEDS.show();
-  delay(10);
+  delay(500);
   Serial.println('\n');
 
   WiFi.softAP(AP_SSID, AP_PSK, 1, false, 2);
@@ -67,9 +83,25 @@ void setup() {
   Serial.print("IP address: \t");
   Serial.println(WiFi.softAPIP());
 
-  server.on("/", handleHome);
-  server.on("/test", []() {
-    server.send(200, "text/plain", "test");
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *req){
+    req->send_P(200, "text/html", index_html);
+  });
+
+  server.on("/update", HTTP_GET, [](AsyncWebServerRequest *req){
+    // slider_val = req->getParam("slider")->value().toInt();
+    String val;
+    String param;
+    val = req->getParam(SLIDER_INPUT)->value();
+    param = SLIDER_INPUT;
+
+    Serial.println(val);
+    Serial.println(param);
+  });
+
+  server.on("/test", HTTP_GET, [](AsyncWebServerRequest *req) {
+    req->send(200, "text/plain", "test");
+    Serial.print("slider_val: ");
+    Serial.println(slider_val);
   });
 
   server.onNotFound(handleNotFound);
@@ -79,14 +111,16 @@ void setup() {
 }
 
 void loop() {
-  for (int i = 0; i++; i < LED_COUNT) {
+  for (int i = 0; i < LED_COUNT; i++) {
     leds[i] = CRGB(0 + slider_val, 255 - slider_val, 0);
     FastLED.show();
     delay(20);
-  }
+  }       
 
-  server.handleClient();
   int connections = WiFi.softAPgetStationNum();
   Serial.println(connections);
-  delay(500);
+  Serial.println(slider_val);
+  delay(1000);
 }
+
+// Ref: https://randomnerdtutorials.com/esp8266-nodemcu-async-web-server-espasyncwebserver-library/
