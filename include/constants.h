715ae9810dbcec4907045e9b2a61c9cf9b865c21
Trixie Roque <trixie.anne.roque@gmail.com>
2022-10-26 20:58:51 -0700

    Separate constants and helper funcs + added unsupervised mode

---
+++ include/constants.h
@@ -0,0 +1,83 @@
+#define CONTROL_PIN D1
+
+// Set Access Point creds
+#ifndef AP_SSID
+#define AP_SSID "trix-sims"
+#define AP_PSK "plumbob-69420"
+#endif
+
+const int LED_COUNT = 13;
+
+// In case something goes wrong...
+const bool DEBUG_MODE = false;
+
+// TODO: Fix this outdated html/js/css...
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html>
+  <head>
+    <title>The Sims Plumbob LED Controller</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+      html { font-family: Arial; display: inline-block; text-align: center; }
+      body { margin: 0px auto; padding-bottom: 15px; }
+      .slider { margin: 0px auto; flex-grow: 1; }
+    </style>
+  </head>
+  <body>
+    <h2>The Sims Plumbob LED Controller</h2>
+    <div align="center">
+      <div id="num_clients"></div>
+      <label for="num_clients">Clients: </label><br>
+
+      <label for="powerState">Power: </label>
+      <input type="checkbox" id="powerState" name="powerState" checked onchange="togglePower(this)" />
+      <br>
+
+      <label for="slider">Color: </label>
+      <input type="range" class="slider" name="slider" min="0" max="255" step="5" value="0" onchange="handleSlider(this)" />
+      <br>
+
+      <label for="brightness">Brightness: </label>
+      <input type="range" class="slider" name="brightness" min="0" max="100" value="100" onchange="handleBrightness(this)" />
+      <br>
+
+      <form action="" onsubmit="handleUnsupervisedMode()">
+        <label for="unsupervisedMode">Unsupervised Mode: </label>
+        <input type="text" name="duration" value="30" id="duration"> min</input>
+        <input type="submit" value="Unsupervised Mode" />
+      </form>
+    </div>
+    <script>
+      function togglePower(e) {
+        let xhr = new XMLHttpRequest();
+        if (e.checked) {
+          xhr.open("GET", "/power?state=1", true);
+        } else {
+          xhr.open("GET", "/power?state=0", true);
+        }
+        xhr.send();
+      }
+
+      function handleSlider(e) {
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET", "/color?value=" + e.value, true);
+        xhr.send();
+        e.preventDefault();
+      }
+
+      function handleBrightness(e) {
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET", "/brightness?value=" + e.value, true);
+        xhr.send();
+        e.preventDefault();
+      }
+
+      function handleUnsupervisedMode() {
+        let xhr = new XMLHttpRequest();
+        let duration = document.getElementById("duration").value;
+        xhr.open("GET", "/unsupervised?duration=" + duration, true);
+        xhr.send();
+      }
+    </script>
+  </body>
+</html>)rawliteral";
--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,14 @@
 #include <ESPAsyncWebServer.h>
 #include <ESPAsyncTCP.h>
 #include <FastLED.h>
+#include <FS.h>
 
-// In case something goes wrong...
-const bool DEBUG_MODE = false;
+#include "constants.h"
+#include "helpers.h"
+
+// TODO: separate html/css/js into data/ dir and use SPIFFS
 
 AsyncWebServer server(80);
-
-// Set Access Point creds
-#ifndef AP_SSID
-#define AP_SSID "trix-sims"
-#define AP_PSK "plumbob-69420"
-#endif
 
 char ssid[] = AP_SSID;
 char password[] = AP_PSK;
@@ -22,23 +19,21 @@ WiFiEventHandler stationConnectedHandler;
 WiFiEventHandler stationDisconnectedHandler;
 uint8_t connections = 0;
 
-#define CONTROL_PIN D1
-
 // TODO: add effects? (like spinning diamond)
 // TODO: add label for number of clients
-
-const int LED_COUNT = 13;
 
 uint8_t color_val = 0;
 uint8_t num_clients = 0;
 uint8_t brightness = 100;
 
 bool poweredOn = true;
-bool leftAlone = false;
+bool unsupervised = false;
+uint8_t unsupervisedDurationInMin;;
+uint8_t colorChangePerSec;
 
 CRGBArray<LED_COUNT> leds;
 
-long currentTime;
+unsigned long currentTime = millis();
 
 void handleHome(AsyncWebServerRequest *req) {
   digitalWrite(CONTROL_PIN, HIGH);
@@ -87,69 +82,32 @@ void handlePowerState(AsyncWebServerRequest *req) {
   req->send(200, "text/plain", "OK");
 }
 
-const char index_html[] PROGMEM = R"rawliteral(
-<!DOCTYPE HTML><html>
-  <head>
-    <title>The Sims Plumbob LED Controller</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <style>
-      html { font-family: Arial; display: inline-block; text-align: center; }
-      body { margin: 0px auto; padding-bottom: 15px; }
-      .slider { flex-grow: 1; }
-    </style>
-  </head>
-  <body>
-    <h2>The Sims Plumbob LED Controller</h2>
-    <div align="center">
-      <div id="num_clients"></div>
-      <label for="num_clients">Clients: </label><br>
+void handleColor(AsyncWebServerRequest *req) {
+  if (req->hasParam("value")) {
+    color_val = req->getParam("value")->value().toInt();
+    if (poweredOn) {
+      powerOn();
+    }
+    unsupervised = false;
+  }
 
-      <input type="checkbox" id="powerState" name="powerState" checked onchange="togglePower(this)" />
-      <label for="powerState">Power: </label><br>
+  if (DEBUG_MODE) {
+    Serial.println(color_val);
+  }
+  req->send(200, "text/plain", "OK");
+}
 
-      <input type="range" class="slider" name="slider" min="0" max="255" step="5" value="0" onchange="handleSlider(this)" />
-      <label for="slider">Slider</label><br>
+void handleBrightness(AsyncWebServerRequest *req) {
+  if (req->hasParam("value")) {
+    brightness = req->getParam("value")->value().toInt();
+    LEDS.setBrightness(brightness);
+    LEDS.show();
+  }
 
-      <input type="range" class="slider" name="brightness" min="0" max="100" value="100" onchange="handleBrightness(this)" />
-      <label for="brightness">Brightness</label><br>
-
-      <form>
-        <input type="text" name="interval" placeholder="30 min" />
-        <input type="submit" value="Submit" />
-      </form>
-    </div>
-    <script>
-      function togglePower(e) {
-        let xhr = new XMLHttpRequest();
-        if (e.checked) {
-          xhr.open("GET", "/power?state=1", true);
-        } else {
-          xhr.open("GET", "/power?state=0", true);
-        }
-        xhr.send();
-      }
-
-      function handleSlider(e) {
-        let xhr = new XMLHttpRequest();
-        xhr.open("GET", "/color?value=" + e.value, true);
-        xhr.send();
-        e.preventDefault();
-      }
-
-      function handleBrightness(e) {
-        let xhr = new XMLHttpRequest();
-        xhr.open("GET", "/brightness?value=" + e.value, true);
-        xhr.send();
-        e.preventDefault();
-      }
-    </script>
-  </body>
-</html>)rawliteral";
- 
-String macToString(const unsigned char* mac) {
-  char buf[20];
-  snprintf(buf, sizeof(buf), "%02x:%02x:%02x:%02x:%02x:%02x", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
-  return String(buf);
+  if (DEBUG_MODE) {
+    Serial.println(brightness);
+  }
+  req->send(200, "text/plain", "OK"); 
 }
 
 void onStationConnected(const WiFiEventSoftAPModeStationConnected& evt) {
@@ -202,37 +160,24 @@ void setup() {
   });
 
   server.on("/power", HTTP_GET, handlePowerState);
+  server.on("/color", HTTP_GET, handleColor);
+  server.on("/brightness", HTTP_GET, handleBrightness);
 
-  server.on("/color", HTTP_GET, [](AsyncWebServerRequest *req){
-    if (req->hasParam("value")) {
-      color_val = req->getParam("value")->value().toInt();
-      if (poweredOn) {
-        powerOn();
-      }
-      leftAlone = false;
+  server.on("/unsupervised", HTTP_GET, [](AsyncWebServerRequest *req){
+    if (req->hasParam("duration")) {
+      unsupervisedDurationInMin = req->getParam("duration")->value().toInt();
+      unsupervised = true;
+      colorChangePerSec = (uint8_t) ceil(255 / (unsupervisedDurationInMin * 60));
+
+      // reset color_val
+      color_val = 0;
+      powerOn();
     }
 
-    if (DEBUG_MODE) {
-      Serial.println(color_val);
+    if(DEBUG_MODE) {
+      Serial.print("unsupervised duration: ");
+      Serial.println(unsupervisedDurationInMin);
     }
-    req->send(200, "text/plain", "OK");
-  });
-
-  server.on("/brightness", HTTP_GET, [](AsyncWebServerRequest *req){
-    if (req->hasParam("value")) {
-      brightness = req->getParam("value")->value().toInt();
-      LEDS.setBrightness(brightness);
-      LEDS.show();
-    }
-
-    if (DEBUG_MODE) {
-      Serial.println(brightness);
-    }
-    req->send(200, "text/plain", "OK");
-  });
-
-  server.on("/get", HTTP_GET, [](AsyncWebServerRequest *req){
-
     req->send(200, "text/plain", "OK");
   });
 
@@ -245,12 +190,17 @@ void setup() {
 }
 
 void loop() {
-  currentTime = millis();
-  if (leftAlone) {
+  if (unsupervised) {
     if (DEBUG_MODE) {
-      Serial.println("Left Alone");
+      Serial.println("Currently unsupervised...");
+      Serial.println("currentTime: " + currentTime);
+      Serial.println("millis(): " + millis());
     }
-
+    if (millis() - currentTime >= 1000 * unsupervisedDurationInMin) { // multiply to extend it maybe???
+      color_val = constrain(color_val + colorChangePerSec, 0, 255);
+      powerOn();
+      currentTime = millis();
+    }
   }
 }
 
